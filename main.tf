94d65544a4b28fc510d861a44cc0c0172a7143ff
Andrzej Pichliński <andrzej.pichlinski@makolab.com>
2021-04-11 23:05:36 +0200

    Task7 - success

--- main.tf
+++ main.tf
@@ -24,3 +24,41 @@ module "instances" {
 module "storage" {
   source     = "./modules/storage"
 }
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 2.5.0"
+
+    project_id   = var.project_id
+    network_name = "terraform-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-central1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-central1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        }
+    ]
+}
+
+resource "google_compute_firewall" "tf-firewall" {
+  name    = "tf-firewall"
+  network = "projects/<PROJECT_ID>/global/networks/terraform-vpc"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_tags = ["web"]
+  source_ranges = ["0.0.0.0/0"]
+}
